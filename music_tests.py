a4ddecb93d3dba52dccc9fa32b4561eeff312882
Rhim Andemichael <rhima@princeton.edu>
2022-07-21 21:57:54 +0000

    unit tests

---
+++ music_tests.py
@@ -0,0 +1,21 @@
+import unittest
+from napster import short_to_id, get_albs, list_genres, random_album
+
+
+class TestNapster(unittest.TestCase):
+    def test_short_to_id(self):
+        self.assertEqual(short_to_id(rock), g.5)
+        self.assertEqual(short_to_id(pop), g.115)
+
+
+    def test_get_albs(self):
+        self.assertEqual(type(get_albs(rock)), type([]))
+        self.assertEqual(len(get_albs(pop)), 2)
+
+    def test_random_album(self):
+        self.assertEqual(type(random_album(albums)), type([]))
+        self.assertEqual(len(random_album(albums)), 2)
+    
+
+if __name__ == '__main__':
+    unittest.main()
