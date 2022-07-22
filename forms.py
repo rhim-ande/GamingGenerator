2e210690c30f289326d54693957b19d6b74fbc03
Bryan Camarillo <bryan07@mail.fresnostate.edu>
2022-07-22 05:14:08 +0000

    updated pep8 style on forms

--- forms.py
+++ forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
+
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
                            validators=[DataRequired(), Length(min=2, max=20)])
@@ -14,13 +15,12 @@ class RegistrationForm(FlaskForm):
 
 
 class LoginForm(FlaskForm):
-   username = StringField('Username', validators=[DataRequired()])
-   password = PasswordField('Password', validators=[DataRequired()])
-   submit = SubmitField('Log In')
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log In')
 
 
 class GameForm(FlaskForm):
     game_genre = SelectField('Game Genres', choices=['mmorpg', 'shooter', 'strategy', 'moba', 'racing', 'sports', 'social', 'sandbox', 'open-world', 'survival', 'pvp', 'pve', 'pixel', 'voxel', 'zombie', 'turn-based', 'first-person', 'third-Person', 'top-down', 'tank', 'space', 'sailing', 'side-scroller', 'superhero', 'permadeath', 'card', 'battle-royale', 'mmo', 'mmofps', 'mmotps', '3d', '2d', 'anime', 'fantasy', 'sci-fi', 'fighting', 'action-rpg', 'action', 'military', 'martial-arts', 'flight', 'low-spec', 'tower-defense', 'horror', 'mmorts'])
     music_genre = SelectField('Music Genres', choices=['pop', 'rock', 'alt-punk', 'rap-hip-hop', 'soul-r-and-b', 'country', 'jazz', 'electronica-dance', 'latin', 'classical', 'metal', 'comedy-spoken-word', 'soundtracks-musicals','vocal-easy-listening', 'children', 'folk', 'christian-gospel', 'blues', 'reggae', 'new-age'])
     submit = SubmitField('Get Game Options')
-
