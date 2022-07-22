1382ba410e555fa0693c0262231bc6614cdb6846
Bryan Camarillo <bryan07@mail.fresnostate.edu>
2022-07-22 04:49:38 +0000

    updated pep8 style on game tests

--- game_tests.py
+++ game_tests.py
@@ -1,13 +1,11 @@
 import unittest
 from games import get_games
 
-
-
 class TestGames(unittest.TestCase):
     def test_get_games(self):
         self.assertEqual(type(get_games(horror)), type([]))
         self.assertEqual(len(get_games(horror)), 3)
 
-
 if __name__ == '__main__':
     unittest.main()
+    
